530b72d4b51a335e1d69157ce9eb9e9cab61fb5d
KREKER <51415630+kreker0001@users.noreply.github.com>
2025-09-11 23:12:54 +0500

    Task3

---
+++ Tyuiu.AndreevIA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AndreevIA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.AndreevIA.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadLine();
+        }
+    }
+}
